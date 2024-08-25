1fdd6c74e3cafc41842f002b7acdde1fd294f6fc
Luiz Carlos <luizcmoficial@gmail.com>
2024-08-25 15:35:10 -0300

    livro-lista working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LivroListaComponent } from './livro-lista/livro-lista.component';
 import { LivroDadosComponent } from './livro-dados/livro-dados.component';
+import { ControleEditoraService } from './controle-editora.service';
+import { ControleLivrosService } from './controle-livros.service';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,11 @@ import { LivroDadosComponent } from './livro-dados/livro-dados.component';
     LivroDadosComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ControleEditoraService, ControleLivrosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
